6caf12e5c0869c7cb60d3ee8cff5aa5d17daa843
Rohan Chandi <mail@eksrow.com>
2024-02-25 19:49:50 +0000

    fix: automatically create avatars dir in script if non-existant

--- scrips/fetch-avatars.sh
+++ scrips/fetch-avatars.sh
@@ -3,6 +3,10 @@
 
 avatar_dir="./public/img/avatars"
 
+if [ ! -d $"$avatar_dir" ]; then
+	mkdir "$avatar_dir"
+fi
+
 for i in {1..20}; do
 	curl -o "$avatar_dir/$i.jpg" "https://avatars.tzador.com/face?gender=male&size=100";
 	sleep 0.5;
